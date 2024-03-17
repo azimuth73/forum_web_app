bebe89355567582efb38ecdefa92629411a9aefe
azimuth73 <playroger2018@gmail.com>
2024-03-18 00:49:37 +0100

    Add more TODOs and tidy things up

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ class BaseThread(BaseModel):
 class ThreadIn(BaseThread):
     pass
 
+# TODO: Make a check to see if ids in pydantic classes are non-positive. (Most likely it works like this, I
+#       don't think ids are ever auto-assigned an id of 0)
+
 
 class ThreadOut(BaseThread):
     id: int
@@ -66,8 +69,13 @@ class ReplyOut(BaseReply):
 
 @app.get(path='/')
 def read_root(db: Session = Depends(get_db)):
-    # TODO: Do some error handling if no threads
+    # TODO: Think of what root's purpose should be, still haven't decided.
+    #       This solution where it returns threads is only temporary.
     return db.query(models.Thread).order_by(models.Thread.created).all()
+
+
+# TODO: Add read_threads route which queries threads according to some parameters.
+#       Something like ascending/descending date of creation, number of threads returned etc.
 
 
 @app.post(path='/threads/', response_model=ThreadOut)
@@ -92,15 +100,20 @@ def read_thread(thread_id: int, db: Session = Depends(get_db)):
     if db_thread is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Thread with ID={thread_id} not found'
+            detail=f'Thread with ID={thread_id} not found.'
         )
 
     return db_thread
 
 
+# TODO: Add read_replies route which queries replies according to some parameters.
+#       Something like ascending/descending date of creation, number of replies returned etc.
+
 @app.post(path='/threads/{thread_id}/replies/', response_model=ReplyOut)
 def create_reply(reply: ReplyIn, db: Session = Depends(get_db)):
     db_reply = models.Reply()
+
+    # TODO: Check if thread with the given id exists, raise exception if it doesn't.
 
     db_reply.thread_id = reply.thread_id
     db_reply.text = reply.text
@@ -115,14 +128,14 @@ def create_reply(reply: ReplyIn, db: Session = Depends(get_db)):
 
 @app.get(path='/threads/{thread_id}/replies/{reply_id}', response_model=ReplyOut)
 def read_reply(thread_id: int, reply_id: int, db: Session = Depends(get_db)):
+    # TODO: A better implementation should probably be if I query for the thread first, and then for
+    #       reply to that thread. This way a different message can be given in the exception depending
+    #       on which is missing.
     db_reply = db.query(models.Reply).filter(models.Reply.id == reply_id, models.Reply.thread_id == thread_id).first()
     if db_reply is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Reply with ID={reply_id} in Thread with ID={thread_id} not found'
+            detail=f'Reply with ID={reply_id} in Thread with ID={thread_id} not found.'
         )
 
     return db_reply
-
-
-
